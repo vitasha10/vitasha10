223e77eeccfc05b1ebdf30b782064aac0ec038d4
vitasha10 <my@vitasha.tk>
2021-11-27 11:46:00 +0000

    hh_commit_

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
-for (( i=1; i <= 100; i++ ))
+for (( i=1; i <= 10; i++ ))
 do
-echo "number is $i"
-
+    for (( i=1; i <= 10; i++ ))
+    do
+        echo "number is $i"
+        echo "number is $i" >> ./.github/log.txt
+        git add *
+        git commit -m "hh_commit_$i"
+    done
+    git push git@github.com:vitasha10\vitasha10.git
 done
