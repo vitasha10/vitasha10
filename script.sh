b7764e56818364e237c13efb10f57a46780f6029
vitasha10 <my@vitasha.tk>
2021-11-27 11:51:15 +0000

    hh_commit_1

--- script.sh
+++ script.sh
@@ -6,7 +6,9 @@ do
         echo "number is $i"
         echo "number is $i" >> ./.github/log.txt
         git add *
+        git config --global user.email "my@vitasha.tk"
+        git config --global user.name "vitasha10"
         git commit -m "hh_commit_$i"
     done
-    git push git@github.com:vitasha10\vitasha10.git
+    git push git@github.com:vitasha10/vitasha10.git
 done
