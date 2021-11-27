9931efc36779ce26523bc1e40d1592c058060750
vitasha10 <my@vitasha.tk>
2021-11-27 12:01:29 +0000

    hh_commit_2

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ do
         git config --global user.email "my@vitasha.tk"
         git config --global user.name "vitasha10"
 
-        git commit -m "hh_commit_$i"
+        git commit -a -m "hh_commit_$i"
 	git push git@github.com:vitasha10/vitasha10.git
 	sleep 5
     done
