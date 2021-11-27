2f349aa6c0bc40fdbfb45dae6096336507cfabca
vitasha10 <my@vitasha.tk>
2021-11-27 12:08:29 +0000

    hh_commit_1

--- script.sh
+++ script.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-for (( i=1; i <= 10; i++ ))
+for (( i=1; i <= 200; i++ ))
 do
-    for (( i=1; i <= 10; i++ ))
-    do
-        echo "number is $i"
-        echo "number is $i" >> ./.github/log.txt
-        git add *
-        git config --global user.email "my@vitasha.tk"
-        git config --global user.name "vitasha10"
-
-        git commit -a -m "hh_commit_$i"
-	git push git@github.com:vitasha10/vitasha10.git
-	sleep 5
-    done
-    sleep 5
+echo "number is $i"
+echo "number is $i" >> ./.github/log.txt
+git add *
+git config --global user.email "my@vitasha.tk"
+git config --global user.name "vitasha10"
+git commit -a -m "hh_commit_$i"
+git push git@github.com:vitasha10/vitasha10.git
+sleep 2
 done
